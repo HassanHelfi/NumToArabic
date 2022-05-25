fe19668a87865015a56acac34737503eb1ef8165
HassanHelfi <82749703+HassanHelfi@users.noreply.github.com>
2022-05-25 16:24:09 +0430

    commented number var

--- useArabicNumber.php
+++ useArabicNumber.php
@@ -3,5 +3,5 @@ include('Number2ArabicWord.php');
 $number_class = new Number2WordArabic;
 
 $number = "100566165681004001025636652298641351";
-$number = "1045568";
+// $number = "1045568";
 echo $number_class->number2Word($number);
