77af0d68d506309c9dbf0c7ed2eb35eaf1a7ac59
HassanHelfi <hbhassanhelfi@gmail.com>
2023-12-01 23:40:25 +0330

    feat: convert old code to package

--- src/NumToArabic.php
+++ src/NumToArabic.php
@@ -1,8 +1,10 @@
 <?php
 
-class Number2WordArabic
+namespace Hassan\NumberToArabic;
+
+class NumToArabic
 {
-    protected $digit = [
+    protected static $digit = [
         ['صفر', 'وا حد', 'إثنان', 'ثلاثة', 'أربعة', 'خمسة', 'ستّة', 'سبعة', 'ثمانية', 'تعسة'],
         ['أحد عشر', 'اثنا عشر', 'ثلاثة عشر', 'أربعة عشر', 'خمسة عشر', 'ستة عشر', 'سبعة عشر', 'ثمانية عشر', 'تسعة عشر'],
         ['عشرة', 'عشرین', 'ثلاثین', 'أربعین', 'خمسین', 'ستین', 'سبعین', 'ثمانین', 'تسعین'],
@@ -12,35 +14,34 @@ class Number2WordArabic
     ];
 
     /**
-     * @param string $number
-     * @return array
-     */
-    public function format(string $number): array
-    {
-        $number = explode('.', str_replace(' ', '', $number));
-        $number[0] = str_split(strrev($number[0]), 3);
-        $groups = array_map("strrev", array_pop($number));
-        return $groups;
-    }
-
-    /**
      * @param String $number
      * @return string
      */
-    public function number2Word(string $number): string
+    public static function number2Word(string $number): string
     {
-        $groups = $this->format($number);
+        $groups = static::format($number);
         $groups_count = count($groups);
-        if ($groups_count == 1) return $this->one($groups);
-        elseif ($groups_count <= 24) return $this->other($groups);
-        else return "...";
+        if ($groups_count == 1) return static::one($groups);
+        elseif ($groups_count <= 24) return static::other($groups);
+        else return "لايمكن تحويل هذا الرقم";
+    }
+
+    /**
+     * @param string $number
+     * @return array
+     */
+    private static function format(string $number): array
+    {
+        $number = str_replace([' ', '.'], ['', ''], $number);
+        $groups = str_split(strrev($number), 3);
+        return array_map("strrev", $groups);
     }
 
     /**
      * @param $groups
      * @return string
      */
-    public function other($groups): string
+    private static function other($groups): string
     {
         for ($i = count($groups) - 1; $i >= 1; $i--) {
             $num = ltrim($groups[$i], '0');
@@ -48,15 +49,15 @@ class Number2WordArabic
             $other_array = $groups;
             array_pop($other_array);
             $other_number = (string)str_replace(',', '', implode(',', array_reverse($other_array)));
-            list($groups, $part) = $this->part($groups, $i);
-            $and = ($groups[$i - 1] == 0) ? '' : $this->digit[5][4];
-            $other = ($other_number == 0) ? '' : $and . $this->number2Word($other_number);
+            list($groups, $part) = static::part($groups, $i);
+            $and = ($groups[$i - 1] == 0) ? '' : static::$digit[5][4];
+            $other = ($other_number == 0) ? '' : $and . static::number2Word($other_number);
             if ($num_count == 1) {
-                if ($num == 1) $num2word = $this->digit[4][$i] . $other;
-                if ($num == 2) $num2word = $this->digit[4][$i] . $this->digit[5][3] . $other;
-                if ($num > 2) $num2word = $this->one([$groups[$i]]) . ' ' . $part . $other;
+                if ($num == 1) $num2word = static::$digit[4][$i] . $other;
+                if ($num == 2) $num2word = static::$digit[4][$i] . static::$digit[5][3] . $other;
+                if ($num > 2) $num2word = static::one([$groups[$i]]) . ' ' . $part . $other;
             } else {
-                $num2word = $this->one([$groups[$i]]) . ' ' . $part . $other;
+                $num2word = static::one([$groups[$i]]) . ' ' . $part . $other;
             }
             return $num2word;
         }
@@ -66,24 +67,24 @@ class Number2WordArabic
      * @param array $group
      * @return string
      */
-    public function one(array $group): string
+    private static function one(array $group): string
     {
         if ($group[0] == 0) $num2word = '';
         $num = ltrim($group[0], '0');
         $num_count = strlen($num);
         $num_arr = array_map('intval', str_split($num));
         if ($num_count == 1) {
-            $num2word = $this->digit[0][$num];
+            $num2word = static::$digit[0][$num];
         } elseif ($num_count == 2) {
-            if ($num_arr[1] == 0 && $num_arr[0] > 0) $num2word = $this->digit[2][$num_arr[0] - 1];
-            if ($num_arr[1] > 0 && $num_arr[0] == 1) $num2word = $this->digit[1][$num_arr[1] - 1];
-            if ($num_arr[1] > 0 && $num_arr[0] > 1) $num2word = $this->digit[0][$num_arr[1]] . $this->digit[5][4] . $this->digit[2][$num_arr[0] - 1];
+            if ($num_arr[1] == 0 && $num_arr[0] > 0) $num2word = static::$digit[2][$num_arr[0] - 1];
+            if ($num_arr[1] > 0 && $num_arr[0] == 1) $num2word = static::$digit[1][$num_arr[1] - 1];
+            if ($num_arr[1] > 0 && $num_arr[0] > 1) $num2word = static::$digit[0][$num_arr[1]] . static::$digit[5][4] . static::$digit[2][$num_arr[0] - 1];
         }
         if ($num_count == 3) {
             if ($num_arr[0] > 0 && $num_arr[1] == 0 && $num_arr[2] == 0) {
-                $num2word = $this->digit[3][$num_arr[0] - 1];
+                $num2word = static::$digit[3][$num_arr[0] - 1];
             } else {
-                $num2word = $this->digit[3][$num_arr[0] - 1] . $this->digit[5][4] . $this->one([$num_arr[1] . $num_arr[2]]);
+                $num2word = static::$digit[3][$num_arr[0] - 1] . static::$digit[5][4] . static::one([$num_arr[1] . $num_arr[2]]);
             }
         }
         return $num2word;
@@ -94,13 +95,13 @@ class Number2WordArabic
      * @param int $i
      * @return array
      */
-    public function part(array $groups, int $i): array
+    private static function part(array $groups, int $i): array
     {
         for ($j = 1; $j <= 2; $j++) {
             if (isset($groups[$j])) {
-                $part = $this->digit[4][$i];
-                if ($groups[$j] <= 10 && $groups[$j] != 0) $part = $this->digit[5][$j - 1];
-                elseif ($groups[$i] <= 10 && $groups[$i] != 0) $part = $this->digit[4][$i] . $this->digit[5][2];
+                $part = static::$digit[4][$i];
+                if ($groups[$j] <= 10 && $groups[$j] != 0) $part = static::$digit[5][$j - 1];
+                elseif ($groups[$i] <= 10 && $groups[$i] != 0) $part = static::$digit[4][$i] . static::$digit[5][2];
             }
         }
         if ($groups[$i] == 0) $part = '';
